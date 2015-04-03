cb27dad161e8a613642462f2ea544186d25c96ff
takahiro-kosaka <takahiro.kosaka@dena.com>
2015-04-03 20:19:25 +0900

    Enter押している間はサッチーが止まるように

--- Assets/script/ChaseUnityChan.cs
+++ Assets/script/ChaseUnityChan.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(Animator))]
 public class ChaseUnityChan : MonoBehaviour {
 
 	public GameObject chaseTarget = null;
@@ -8,14 +9,28 @@ public class ChaseUnityChan : MonoBehaviour {
 	private Vector3 vec;
 	private float speed = 0.03f;
 
+	private Animator animator;
+	private int damagingId;
+
 	// Use this for initialization
 	void Start () {
 		targetTransform = chaseTarget.transform;
+
+		animator = GetComponent<Animator> ();
+		damagingId = Animator.StringToHash("damaging");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(targetTransform.position - transform.position), 0.07f);
-		transform.position += transform.forward * speed;
+		if (animator.GetCurrentAnimatorStateInfo (0).IsName ("Chase")) {
+			transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (targetTransform.position - transform.position), 0.07f);
+			transform.position += transform.forward * speed;
+		}
+
+		if (Input.GetKey (KeyCode.Return)) {
+			animator.SetBool (damagingId, true);
+		} else {
+			animator.SetBool (damagingId, false);
+		}
 	}
 }
