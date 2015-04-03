73387c62223c48841de38095ff43e0fae9fffecc
takahiro-kosaka <takahiro.kosaka@dena.com>
2015-04-03 18:49:03 +0900

    enable to chase unityChan

--- Assets/script/ChaseUnityChan.cs
+++ Assets/script/ChaseUnityChan.cs
@@ -1,15 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class ChaseUnityChan : MonoBehaviour {
+
+	public GameObject chaseTarget = null;
+	private Transform targetTransform;
+	private Vector3 vec;
+	private float speed = 0.03f;
 
 	// Use this for initialization
 	void Start () {
-	
+		targetTransform = chaseTarget.transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(targetTransform.position - transform.position), 0.07f);
+		transform.position += transform.forward * speed;
 	}
 }
