f65104172e55dbfad81b91d5b42306428e86352a
takahiro-kosaka <takahiro.kosaka@dena.com>
2015-04-08 20:49:52 +0900

    壁から出てくるスクリプト追加

---
+++ Assets/script/ClippingController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClippingController : MonoBehaviour {
+
+	public GameObject farClipTarget = null;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (farClipTarget == null) {
+			return;
+		}
+
+		float distance = Vector3.Distance (Camera.main.transform.position, farClipTarget.transform.position);
+		Camera.main.farClipPlane = distance + 0.01f;
+	}
+	
+}
