ab7c5f7132a534bb186eb35b97b3a1498d1c6e4b
takahiro-kosaka <takahiro.kosaka@dena.com>
2015-04-18 00:08:49 +0900

    ディフューズ色調調整済み

--- Assets/script/WebCamBehaviourScript.cs
+++ Assets/script/WebCamBehaviourScript.cs
@@ -6,6 +6,7 @@ public class WebCamBehaviourScript : MonoBehaviour
 	public int Width = 1920;
 	public int Height = 1080;
 	public int FPS = 30;
+	public bool Mirror = false;
 	
 	// Use this for initialization
 	void Start()
@@ -15,7 +16,13 @@ public class WebCamBehaviourScript : MonoBehaviour
 			Debug.LogError( "Webカメラが検出できませんでした。" );
 			return;
 		}
-		
+
+		if ( Mirror ) {
+			transform.localScale = new Vector3( -transform.localScale.x,
+			                                   transform.localScale.y,
+			                                   transform.localScale.z );
+		}
+
 		// WebCamテクスチャを作成する
 		var webcamTexture = new WebCamTexture( Width, Height, FPS );
 		GetComponent<Renderer>().material.mainTexture = webcamTexture;
@@ -25,6 +32,6 @@ public class WebCamBehaviourScript : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		
+
 	}
 }
